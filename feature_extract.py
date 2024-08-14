4a6d7de6a619250071299fa8412edb4f4d491cb1
James Jin <jamesjin0516@gmail.com>
2024-08-13 20:32:37 -0400

    Save & load model epoch and score; expose encodings & parameters

--- feature_extract.py
+++ feature_extract.py
@@ -9,7 +9,7 @@ class MixVPRFeatureExtractor:
     def __init__(self, root, content, pipeline=False):
         self.in_h, self.in_w = [size // 16 for size in content["resized_img_size"]]
         self.device = "cuda" if content["cuda"] else "cpu"
-        self.agg_dim = self.load_model(root, content, pipeline)
+        self.saved_state, self.agg_dim = self.load_model(root, content, pipeline)
         self.img_transform = tvf.Resize((320, 320), interpolation=tvf.InterpolationMode.BICUBIC)
     
     def load_model(self, root, content, pipeline):
@@ -26,18 +26,40 @@ class MixVPRFeatureExtractor:
                                     'out_rows': 4},
                         )
 
-        state_dict = torch.load(join(root, join(content["ckpt_path"], "model_best.pth") if pipeline
+        saved_state = torch.load(join(root, join(content["ckpt_path"], "model_best.pth") if pipeline
                                      else content["ckpt_path"]), map_location=self.device)
-        self.model.load_state_dict(state_dict)
+        if saved_state.keys() != {"epoch", "best_score", "state_dict"}:
+            saved_state = {"epoch": 0, "best_score": 0, "state_dict": saved_state}
+        self.model.load_state_dict(saved_state["state_dict"])
 
         self.model.eval().to(self.device)
         print(f"Loaded MixVPR model from {content['ckpt_path']} Successfully!")
-        return self.model.agg_config["out_rows"] * self.model.agg_config["out_channels"]
+        return saved_state, self.model.agg_config["out_rows"] * self.model.agg_config["out_channels"]
     
     def __call__(self, images):
-        with torch.no_grad():
-            encodings, descriptors = self.model(self.img_transform(images))
-            return encodings.detach().cpu(), descriptors.detach().cpu()
+        encodings, descriptors = self.model(self.img_transform(images))
+        return encodings, descriptors
     
+    def set_train(self, is_train):
+        self.model.train(is_train)
+    
+    def torch_compile(self, float32=False, **compile_args):
+        self.model = torch.compile(self.model, **compile_args)
+        if float32:
+            self.model.to(torch.float32)
+    
+    def save_state(self, save_path, new_state):
+        new_state["state_dict"] = self.model.state_dict()
+        torch.save(new_state, save_path)
+    
+    @property
+    def last_epoch(self): return self.saved_state["epoch"]
+
+    @property
+    def best_score(self): return self.saved_state["best_score"]
+
+    @property
+    def parameters(self): return self.model.parameters()
+
     @property
     def feature_length(self): return self.agg_dim
